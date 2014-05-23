ad88406029bb6a582891415ade11fa72bcdc7112
olivekelpie <fumikom@gmail.com>
2014-05-22 21:13:54 -0600

    update

--- script.js
+++ script.js
@@ -30,25 +30,73 @@ $(document).ready(function() {
 	
 	//shuffle the deck
 	
-	
+	deck = _.shuffle(deck);
+
+	console.log(deck);
+
+
 	var cards_player_1 = [];
 	var cards_player_2 = [];
 	//divide out the cards into the two arrays
+
+	var devide = function(deck){
+		
+		for (var i = 0; i<deck.length; i++) {
+			var card = deck[i];
+			if (i%2 === 0){
+				cards_player_1.push(card);
+			}else{
+				cards_player_2.push(card);
+			}
+		
+		}
+			
+	}
+
+		
 	
+		devide(deck);
+		console.log(cards_player_1);
+		console.log(cards_player_2);
+			
 	
 	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
+	// war(opponent_1.pop(), opponent_2.pop())
+			var card1 = cards_player_1[0];
+			var card2 = cards_player_2[0];
 	function war() {
-	}
+				if (card1.number > card2.number){return "Opponent"}
+				else if (card1.number < card2.number){return "Me"}
+				else if (card1.number === card2.number){return "false"}
+			}
 	
 	
+	console.log(war());
 	//create a play function
 		//compare the cards
 		//give the winner both cards (at end of deck)
 	function play() {
-		
+	
+		if(war() === "Opponent") {
+			cards_player_1.push(card1, card2);
+			cards_player_1.shift();
+			cards_player_2.shift();
+		}
+		else if (war() === "Me" ){
+			cards_player_2.push(card1, card2);
+			cards_player_1.shift();
+			cards_player_2.shift();
+		}
+		else if (war() === "false"){
+			cards_player_1.push(card1);
+			cards_player_2.push(card2);
+			cards_player_1.shift();
+			cards_player_2.shift();
+		}
 		//this function (defined below) will continue to the next turn
 		advance();
 	}
+
 	
 	function advance() {
 		//take the top two cards and display them
